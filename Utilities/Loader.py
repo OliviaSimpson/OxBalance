0403ad360bcada561d73a93479e7d935f0382eef
Olivia Simpson <olivia.simpson@linacre.ox.ac.uk>
2023-02-16 18:31:50 +0000

    Updating data loader documentation and methods

--- Utilities/Loader.py
+++ Utilities/Loader.py
@@ -22,22 +22,21 @@ class DataLoader():
 
     def __init__(self, input_path=None, verbose=False) -> None:
 
+        # set class variables
         self.data_file_path = None
         self.verbose = verbose
 
-
-        if input_path != None:
-            if '.h5' in input_path: # if the data file is an h5
+        
+        if (input_path != None) and ('.h5' in input_path): # if a path is given and it contains the .h5 extension
                 self.data_file_path = input_path
         if self.data_file_path == None:     # if data file path is still uset, find the data file from the locator
             self.find_data_file_from_locator(input_path)
         
+        # test data file path
         if self.test_data_file() is True:
             if self.verbose == True:
                 print(f"Data File Path: {self.data_file_path}")
         
-
-
     def find_data_file_from_locator(self, data_locator_path):
         """Locates data file
 
@@ -61,7 +60,7 @@ class DataLoader():
             data_locator_path = os.path.join('data_locator.txt')  # set to defult path
             self.data_file_path = os.path.normpath(data_locator_path)   # convert to os.path normal path 
 
-        # ELSE (if path not None) use given path)
+        # ELSE (if path not None) use passed path)
         
         try:
             # Open Data locator and read path to data file
@@ -74,36 +73,90 @@ class DataLoader():
             # If file not found present error message
             raise Exception(f"Data locator file not found at the path: \"{data_locator_path}\"")
 
-        # if os.path.exists(self.data_file_path):
-        #     return self.data_file_path
-        # else:
-        #     data_file = open(self.data_file_path, "r")
-        #     print(data_file.readline())
-        #     data_file.close()
-
-        #     raise Exception(f"The selected data file (\"{self.data_file_path}\") does not exist")
-
-
-
     def test_data_file(self):
-        passingtests = []
-        passingtests.append(os.path.exists(self.data_file_path))
-        try:
-                data_file = open(self.data_file_path, "r")
-                #     print(data_file.readline())
-                data_file.close()
-                passingtests.append(True)
-        except:
-            passingtests.append(False)
-            raise Exception(f"Data file: \"{self.data_file_path}\" will not open")
-        
-        if self.verbose == True:
-            if passingtests == [True, True]:
-                print(f"Data File: {self.data_file_path} appers good")
-            elif passingtests[0] == False:
-                print(f"Data File: {self.data_file_path} dosent exist")
-            else:
-                print(f"Data File: {self.data_file_path} dosent open")
+        """Tests data file
 
-data = DataLoader(verbose=True)
-# data.show_data_file_path()
+        Tests that the file path for the data file exists and that the data file can be opened and closed.
+
+        Args:
+            
+
+        Returns:
+            Bool: True if file exists and can be opened, False if path does not exist or file causes error when opening
+
+        Raises:
+            Data File: {self.data_file_path} does not exist
+            Data File: {self.data_file_path} does not  open
+            Unexpected test result combination: {passingtests}
+        """
+        passingtests = []   # array variable to hold the results of file path and file open tests
+        passingtests.append(os.path.exists(self.data_file_path)) # Test if file path exists
+
+        # If path does not exist
+        if passingtests[0] == False:
+            if self.verbose:
+                print("DATA FILE TEST FAIL: Data file path existance test Failed")
+            raise Exception(f"Data File: {self.data_file_path} does not exist")
+
+        # If path exists
+        else:
+            if self.verbose:
+                print("DATA FILE TEST PASS: Data ffile path existance test PASSED")
+
+            # Test opening/closing file
+            try:
+                    data_file = open(self.data_file_path, "r")
+                    data_file.close()
+                    passingtests.append(True)
+            except:
+                # If opening/closing file causes error
+                passingtests.append(False)
+                if self.verbose:
+                    print("DATA FILE TEST FAIL: Data file Open/Close test Failed")
+                raise Exception(f"Data File: {self.data_file_path} does not  open")
+                return False
+            
+            # Both Tests Passing            
+            if passingtests == [True, True]:
+                if self.verbose:
+                    print("DATA FILE TEST PASS: Data file Open/Close test PASSED")
+                return True
+            else:
+                # This situation should never occore
+                raise Exception(f"Unexpected test result combination: {passingtests}")
+                
+    def toggle_verbose(self, newstate = None):
+        """Toggles dataloader verbose
+
+        Switches between verbose and not verbose, if given a new state (bool) will switch verbose to be the given state.
+
+        Args:
+            newstate=None: (optional) if provided this will change the classe's verboseness to match this value
+
+        Returns:
+
+        Raises:
+
+        """
+        # if new state is given set verbose to new state:
+        if type(newstate) == "bool":
+            self.verbose = newstate
+        # if a new state is not given toggle verbose
+        else:
+            self.verbose ^= self.verbose
+
+    # def change_data_file(input_path=None,):
+    #     if (input_path != None) and ('.h5' in input_path): # if a path is given and it contains the .h5 extension
+    #             self.data_file_path = input_path
+    #     if self.data_file_path == None:     # if data file path is still uset, find the data file from the locator
+    #         self.find_data_file_from_locator(input_path)
+        
+    #     # test data file path
+    #     if self.test_data_file() is True:
+    #         if self.verbose == True:
+    #             print(f"Data File Path: {self.data_file_path}")
+
+# junk code
+# print(type(True))
+# data = DataLoader(verbose=True)
+# # data.show_data_file_path()
