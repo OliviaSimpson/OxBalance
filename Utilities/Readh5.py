82adcc01e9ecf34b3877820b15a5ac259594330e
Olivia Simpson <olivia.simpson@linacre.ox.ac.uk>
2023-02-16 18:54:38 +0000

    First wrap of h5py

--- Utilities/Readh5.py
+++ Utilities/Readh5.py
@@ -1,15 +1,33 @@
 import h5py
 from Loader import DataLoader
 
-data = DataLoader(verbose=True)
+
+# with h5py.File(data_source.data_file_path, "r") as datafile:
+#     print("Keys: %s" % datafile.keys())
+#     a_group_key = list(datafile.keys())[0]
+
+#     print(type(datafile[a_group_key])) 
+
+#     data = list(datafile[a_group_key])
+#     print(data)
+
+class h5_handler():
+
+    def __init__(self, source) -> None:
+       self.set_source(source)
+       self.open_file()
 
 
+    def set_source(self, source):
+        self.source = source
 
-with h5py.File(data.data_file_path, "r") as datafile:
-    print("Keys: %s" % datafile.keys())
-    a_group_key = list(datafile.keys())[0]
+    def open_file(self):
+        datafile = h5py.File(self.source, 'r')
 
-    print(type(datafile[a_group_key])) 
+    def get_keys(self):
+        print(f"Keys: {self.datafile.keys()}")
 
-    data = list(datafile[a_group_key])
-    print(data)
+
+data_source = DataLoader()
+dataset = h5_handler(data_source.data_file_path)
+dataset.get_keys()
