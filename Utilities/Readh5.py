5bafc5d8f3a3eb34b93953ca06ad744f4926cd7c
Olivia Simpson <olivia.simpson@linacre.ox.ac.uk>
2023-02-20 12:15:41 +0000

    unknown change

--- Utilities/Readh5.py
+++ Utilities/Readh5.py
@@ -7,10 +7,11 @@ import pandas as pd
 
 class h5_handler():
 
-    def __init__(self, source) -> None:
-       self.set_source(source)
-       self.open_file()
-
+    def __init__(self, source,verbose=False) -> None:
+        self.verbose = verbose
+        self.set_source(source)
+        self.open_file()
+        self.subjectlist = None
 
     def set_source(self, source):
         self.source = source
@@ -23,29 +24,52 @@ class h5_handler():
     def close_file(self):
         self.datafile.close()
     
-    def get_subject_list(self, key):
+    def get_subject_list(self):
         print(self.datafile)
         # hdf = pd.HDFStore(self.source, mode='r')
-        subjectlist = pd.read_hdf(self.source, key=list(self.datafile.keys())[-1], mode='r')
-        return(subjectlist)
+        self.subjectlist = pd.read_hdf(self.source, key=list(self.datafile.keys())[-1], mode='r')
+        return(self.subjectlist)
+    
+    def subject_code_from_index(self, index):
+        if type(self.subjectlist) == None:
+            self.get_subject_list()
+        if self.verbose is True:
+            print(f"Subject code: {self.subjectlist.iloc[index].tolist()}")
+        return self.subjectlist.iloc[index]
+    
+    def subject_index_from_code(self, code):
+        if type(self.subjectlist) == None:
+            self.get_subject_list()
+        SubjectIndex = self.subjectlist.index[self.subjectlist['subject'] == code].tolist()
+        if self.verbose is True:
+            if len(SubjectIndex) == 1:
+                print(f"One subject index result: {SubjectIndex}")
+            elif len(SubjectIndex) == 0:
+                print(f"No subject index results")
+            else:
+                print(f"Multiple subject index results: {SubjectIndex}")
+        return SubjectIndex
 
 
 def createdummydata():
     hdf = pd.HDFStore('dummy_h5.h5')
-    write_data = pd.DataFrame(np.random.rand(5,3))
-    hdf.put('key1', write_data)
-    write_2_data = pd.DataFrame(np.random.rand(3,3))
-    hdf.put('key2', write_2_data)
-    write_3_data = pd.DataFrame(np.random.rand(7,7))
-    hdf.put('key3', write_3_data)
+    hdf.put('abcd', pd.DataFrame(np.random.rand(3,3)))
+    hdf.put('abcd/active_test_info', pd.DataFrame(np.random.rand(2,1)))
+    hdf.put('abcd/smartphone_device_location', pd.DataFrame(np.random.rand(6,1)))
+    hdf.put('abcd/smartphone:A/testid:0/device_id:a/acc', pd.DataFrame(np.random.rand(100,3)))
+    hdf.put('efgh', pd.DataFrame(np.random.rand(3,3)))
+    hdf.put('ijkl', pd.DataFrame(np.random.rand(3,3)))
+    hdf.put('mnop', pd.DataFrame(np.random.rand(3,3)))
+    slist = pd.DataFrame(['abcd', 'key2', 'key3', 'key4'], columns=['subject'])
+    hdf.put('subjectlist', slist)
     hdf.close()
 
 
-
+createdummydata()
 source = DataLoader().data_file_path
 
-trialdata = h5_handler(source)
-print(trialdata.get_subject_list('key1'))
-print(trialdata.get_subject_list('key2'))
-print(trialdata.get_subject_list('key3'))
+trialdata = h5_handler(source, verbose=True)
+print(trialdata.get_subject_list())
 trialdata.close_file()
+trialdata.subject_code_from_index(1)
+trialdata.subject_index_from_code('key2')
